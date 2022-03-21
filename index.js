efc68af778a9b69e02979b0dfaa1c8e570cb4fcf
Johnathan <jocuog@gmail.com>
2022-03-21 16:25:02 -0600

    commit tree cf5a6046a77624ca69032b60e6e073396624f2d0

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded' , () => {
+    console.log('the dom has loaded')
+    document.querySelector('#text').textContent = "This is really cool!"
+
+})
+console.log("fires when index.js loads")
